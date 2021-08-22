94db9415553ba391a9f2e5209588785abe713bf0
Jetal C. Mali <malijetal1234@gmail.com>
2021-08-23 00:31:50 +0530

    HTML Learning

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <h2>This is HTML Forms</h2>
       <form action="backend.js">
         <div>
-          Name: <input type="text" />
+          <label htmlFor="name">Name: </label>
+          <input type="text" name="myName" id="name" />
         </div>
         <div>
           Role: <input type="text" />
@@ -29,9 +30,44 @@ function App() {
           <input type="radio" name="myGender" />
         </div>
         <div>
-          <input type="submit" alue="Submit Now" />
+          <label htmlFor="car" name="car">
+            Car
+          </label>
+          <select name="myCar" id="car">
+            <option value="ind">Indica</option>
+            <option value="swft" selected>
+              Swift
+            </option>
+          </select>
+        </div>
+        <div>
+          Write about yourself:{" "}
+          <textarea name="myText" cols="30" rows="10"></textarea>
+        </div>
+        <div>
+          <input type="submit" value="Submit Now" />
+          <input type="reset" value="Reset Now" />
         </div>
       </form>
+      <br />
+      {/* Inline Element */}
+      <h2>Inline Element</h2>
+      <span>Span</span>
+      <a href="#">Anchor</a>
+      <input type="text" />
+      Input
+      <img src="../public/logo192.png" alt="Image" />
+      {/* Block Element */}
+      <h2>Block Element</h2>
+      <h3>Heading</h3>
+      <p>Paragragh</p>
+      <div>Division</div>
+      {/* HTML Entities */}
+      <p>My name is Jetal. &nbsp; I am a programmer.</p>
+      <p>I want to write &lt;p&gt;</p>
+      <p>Pound is written like &pound;</p>
+      <p>Copyright is written like &copy;</p>
+      <p>Empty Character is written like &#8203;</p>
     </div>
   );
 }
